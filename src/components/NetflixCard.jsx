c2326c8a514cd0eba50ef64cfe37c5cdc75e9241
tashrif-miyaji <tashrifmiyaji@gmail.com>
2024-11-19 04:24:21 +0600

    using css module

--- src/components/NetflixCard.jsx
+++ src/components/NetflixCard.jsx
@@ -1,13 +1,19 @@
 import netflixSeriesData from "../api/data.json";
 import { NetflixCardList } from "./NetflixCardList";
-import "../css/netflix.css";
+
+// in the react css file is global file, if i use global css file in the parent jsx file then it's class will be accessible in the all children file.
+// global css file import system like this (import "../css/netflix.css";). on the other hand css module file. module file import system like this (import style from "../css/netflix.module.css";)
+
+// if i use css module then Each class will be treated like a property of the object. and it not accessible in the children file, If I want to use then I have to import each file anew.
+// and css module And behind the scene css module makes each class unique.(ist beauty of css module)
+import style from "../css/netflix.module.css"
 
 export const Netflix = () => {
 	return (
 		<>
-			<div className="container">
+			<div className={style.container}>
 				<h1>List of Best Netflix Series</h1>
-				<ul className="grid grid-three-clos">
+				<ul className={`${style.grid} ${style["grid-three-clos"]}`}>
 					{netflixSeriesData.map((item) => (
 						<NetflixCardList item={item} key={item.id} />
 					))}
@@ -16,26 +22,3 @@ export const Netflix = () => {
 		</>
 	);
 };
-
-// export const Netflix = () => {
-// 	return (
-// 		<>
-// 		<ul>
-// 			{
-// 				netflixSeriesData.map((item)=>{
-// 					return(
-// 						<li key={item.id}>
-// 							<img src={item.img} alt="photo" width="40%" height="40%" />
-// 							<p>Name: {item.name}</p>
-// 							<p>Description: {item.description}</p>
-// 							<p>{item.rating}</p>
-// 							<button><a href={item.url} target="_blank">watch now</a></button>
-// 						</li>
-// 					)
-// 				})
-// 			}
-// 		</ul>
-
-// 		</>
-// 	);
-// };
--- src/components/NetflixCardList.jsx
+++ src/components/NetflixCardList.jsx
@@ -1,3 +1,11 @@
+// in the react css file is global file, if i use global css file in the parent jsx file then it's class will be accessible in the all children file.
+// global css file import system like this (import "../css/netflix.css";). on the other hand css module file. module file import system like this (import style from "../css/netflix.module.css";)
+
+// if i use css module then Each class will be treated like a property of the object. and it not accessible in the children file, If I want to use then I have to import each file anew.
+// and css module And behind the scene css module makes each class unique.(ist beauty of css module)
+
+import style from "../css/netflix.module.css";
+
 export const NetflixCardList = (props) => {
 	const { name, img_url, rating, description, cast, genre, watch_url } =
 		props.item;
@@ -6,19 +14,19 @@ export const NetflixCardList = (props) => {
 	const btnStyle = {
 		width: "10rem",
 		fontSize: "1rem",
-		backgroundColor: `${rating > 8.2 ? "#DB2929": "#3136BD"}`,
+		backgroundColor: `${rating > 8.2 ? "#DB2929" : "#3136BD"}`,
 		color: "white",
 		borderRadius: "10px",
 	};
 
 	return (
-		<li className="card">
-			<div className="card-img">
+		<li className={style.card}>
+			<div className={style["card-img"]}>
 				<img src={img_url} alt={name} width="40%" height="40%" />
 			</div>
-			<div className="card-content">
+			<div className={style["card-content"]}>
 				<h2>Name: {name}</h2>
-				<h3>Rating:<span className={`rating ${rating > 8.2 ? "rating-up": "rating-down"}`} >{rating}</span></h3>
+				<h3>Rating:<span className={`${style.rating} ${rating > 8.2 ? style["rating-up"] : style["rating-down"]}`}>{rating}</span></h3>
 				<p>Description: {description}</p>
 				<p>Genre: {genre}</p>
 				<p>Cast: {cast}</p>
