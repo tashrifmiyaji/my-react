ade49425031c0fb86375e2e880e036479c5b1412
tashrif-miyaji <tashrifmiyaji@gmail.com>
2025-06-24 15:56:20 +0600

    Add active route styling using isActive in NavLink

--- src/components/Projects/React Movie website/layOut/Header.jsx
+++ src/components/Projects/React Movie website/layOut/Header.jsx
@@ -1,35 +1,59 @@
 import { NavLink } from "react-router-dom";
 
 export const Header = () => {
+	const getNavLinkStyle = ({ isActive }) => {
+		return {
+			color: isActive ? "green" : "black",
+		};
+	};
+
 	return (
 		<>
 			<header className="section-navbar">
 				<div className="container">
 					<div className="navbar-brand">
-						<a to="index">
-							<p>@Tashrif-miyaji</p>
-						</a>
+						<NavLink to="index">
+							<p>Tashrif-miyaji</p>
+						</NavLink>
 					</div>
 
 					<nav className="navbar">
 						<ul>
-							<li className="">
-								<NavLink to="/" className={""}>
+							<li className="nav-item">
+								<NavLink
+									to="/"
+									className={({ isActive }) =>
+										isActive
+											? "!text-teal-600"
+											: "!text-black"
+									}
+								>
 									Home
 								</NavLink>
 							</li>
-							<li className="">
-								<NavLink to="/about" className="black">
+							<li className="nav-item">
+								<NavLink
+									to="/about"
+									style={({ isActive }) => {
+										return {
+											color: isActive ? "red" : "black",
+										};
+									}}
+								>
 									about
 								</NavLink>
 							</li>
-							<li className="">
-								<NavLink to="movie" className="">
+							<li className="nav-item">
+								<NavLink
+									to="movie"
+									className="nav-link"
+									style={getNavLinkStyle}
+								>
 									movies
 								</NavLink>
 							</li>
-							<li className="">
-								<NavLink to="contact" className="">
+							<li className="nav-item">
+								<NavLink to="contact" style={getNavLinkStyle}>
 									contact
 								</NavLink>
 							</li>
