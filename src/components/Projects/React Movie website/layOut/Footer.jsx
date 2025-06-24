64d2dbe04d9ec486882f939c70cc95f644aedec6
tashrif-miyaji <tashrifmiyaji@gmail.com>
2025-06-24 16:34:29 +0600

    add website Footer

--- src/components/Projects/React Movie website/layOut/Footer.jsx
+++ src/components/Projects/React Movie website/layOut/Footer.jsx
@@ -1,5 +1,51 @@
 export const Footer = () => {
-    return (
-        <h1>footer</h1>
-    )
-}
+	return (
+		<footer className="section-footer">
+			<div className="footer-container container">
+				<div className="content_1">
+					<img src="./images/logo.png" alt="logo" />
+					<p>
+						Welcome to Tashrif Movie Store, your ultimate
+						destination for all types of movies!
+					</p>
+					<img
+						src="https://i.postimg.cc/Nj9dgJ98/cards.png"
+						alt="cards"
+					/>
+				</div>
+				<div className="content_2">
+					<h4>Other Things</h4>
+					<a href="#">Songs</a>
+					<a href="#">drama</a>
+					<a href="#">series</a>
+					<a href="#">short films</a>
+				</div>
+				<div className="content_3">
+					<h4>Experience</h4>
+					<a href="./contact.html">Contact Us</a>
+					<a href="" target="_blank">
+						Payment Method
+					</a>
+				</div>
+				<div className="content_4">
+					<h4>NEWSLETTER</h4>
+					<p>
+						Be the first to know about new
+						<br />
+						arrivals!
+					</p>
+					<div className="f-mail">
+						<input type="email" placeholder="Your Email" />
+						<i className="bx bx-envelope"></i>
+					</div>
+					<hr />
+				</div>
+			</div>
+			<div className="f-design">
+				<div className="f-design-txt">
+					<p>Design and Code by @tashrif-miyaji</p>
+				</div>
+			</div>
+		</footer>
+	);
+};
