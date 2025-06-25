2298149ae7faa35e24cd777dc14996b2203ed2c2
tashrif-miyaji <tashrifmiyaji@gmail.com>
2025-06-25 22:23:55 +0600

    create loader with useNavigation Hook.

--- src/components/Projects/React Movie website/layOut/AppLayOut.jsx
+++ src/components/Projects/React Movie website/layOut/AppLayOut.jsx
@@ -1,8 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router";
 import { Footer } from "./Footer";
 import { Header } from "./Header";
+import { Loading } from "./Loading";
 
 const AppLayOut = () => {
+	const loading = useNavigation();
+
+	if (loading.state === "loading") return <Loading />;
+
 	return (
 		<>
 			<Header />
