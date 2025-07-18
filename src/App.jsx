d03b18390d5ca3066bc071f0983112deac92a557
tashrif-miyaji <tashrifmiyaji@gmail.com>
2025-07-18 06:56:54 +0600

    change project folder

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import "./index.css";
 // import { Practice1 } from "./components/Derived State/practice/practice1";
 // import { DerivedState } from "./components/Derived State/Derived State";
 //  import { LiftingStateUp } from "./components/Lifting State Up/LiftingStateUp";
-//  import { ToggleSwitch } from "./components/Projects/Toggle Switch/Toggle Switch";
-//  import { Todo } from "./components/Projects/Todo App/Todo";
+//  import { ToggleSwitch } from "./Projects/Toggle Switch/Toggle Switch";
+//  import { Todo } from "./Projects/Todo App/Todo";
 //  import { Counter } from "./components/Hook/Counter with useState";
 // import { SignUpHandlingWithHardWay } from "./components/Hook/Form Handling/sign-upHandlingWithHardWay";
 // import { SignUpHandlingWithRightWay } from "./components/Hook/Form Handling/sign-upHandlingWithRightWay";
@@ -15,7 +15,7 @@ import "./index.css";
 // import { ReactUseEffect2 } from "./components/Hook/useEffect/useEffect-2";
 // import {CleanUp} from "./components/Hook/useEffect/cleanUp"
 // import {FetchingData} from "./components/Fetching API Data/fetchingData"
-// import { Pokemon } from "./components/Projects/Pokemon Cards/pokemon"
+// import { Pokemon } from "./Projects/Pokemon Cards/pokemon"
 // import { UseRef } from "./components/Hook/useRef/index";
 // import { ForwardRef } from "./components/Hook/useRef/forwardRef";
 //
@@ -39,10 +39,10 @@ import "./index.css";
 // import { UseMemo } from "./components/Hook/Memo/useMemo/useMemo"
 // import { CounterWithReactMemoAndUseMemo } from "./components/Hook/Memo/memo with useMemo/Counter"
 // import { UseCAllBack } from "./components/Hook/Memo/useCallBack/UseCallBack"
-// import ReactMovieWebsite from "./components/Projects/React Movie website/app";
-// import React_App_With_Axios_For_CRUD_Operations from "./components//Projects/React App with Axios For CRUD Operations/app"
-// import { Accordion } from "./components/Projects/Accordion App/components/UI/Accordion";
-import WorldExplore from "./components/Projects/world-explore/app";
+// import ReactMovieWebsite from "./Projects/React Movie website/app";
+// import React_App_With_Axios_For_CRUD_Operations from "./component/React App with Axios For CRUD Operations/app"
+// import { Accordion } from "./Projects/Accordion App/components/UI/Accordion";
+import WorldExplore from "./Projects/world-explore/app";
 
 function App() {
 	return (
